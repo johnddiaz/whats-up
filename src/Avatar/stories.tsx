2f9f3b549ba4655c9cb7e9f94c54778da59a23db
johnddiaz <jchubbs95@gmail.com>
2021-06-01 14:58:23 -0500

    added Avatar story

---
+++ src/Avatar/stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import { Meta } from '@storybook/react'
+
+import Avatar from './index'
+
+export default {
+    title: 'Components/Avatar',
+    component: Avatar,
+} as Meta
+
+export const Placeholder = () => <Avatar />
