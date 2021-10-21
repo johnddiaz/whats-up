5835d9c60d497019376a7d6a56dd9c30f3c42451
johnddiaz <jchubbs95@gmail.com>
2021-10-21 17:02:27 -0600

    quick update to login page - will improve later

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Header from '../Header'
 
 interface Props {
     showSignInPopup: (method: 'google' | 'github') => void
@@ -6,27 +7,43 @@ interface Props {
 
 export function SignIn(props: Props) {
     return (
-        <div
-            style={{
-                height: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column',
-            }}
-        >
-            <button
-                style={{ margin: '4px' }}
-                onClick={() => props.showSignInPopup('google')}
+        <div style={{ height: '100%' }}>
+            <Header>
+                <h3
+                    style={{
+                        marginLeft: '16px',
+                        fontSize: '1.3em',
+                    }}
+                >
+                    Whats Up
+                </h3>
+            </Header>
+            <div
+                style={{
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                }}
             >
-                Sign in with Google
-            </button>
-            <button
-                style={{ margin: '4px' }}
-                onClick={() => props.showSignInPopup('github')}
-            >
-                Sign in with Github
-            </button>
+                <button
+                    style={{
+                        margin: '4px',
+                        fontSize: '24px',
+                        marginBottom: '16px',
+                    }}
+                    onClick={() => props.showSignInPopup('google')}
+                >
+                    Sign in with Google
+                </button>
+                <button
+                    style={{ margin: '4px', fontSize: '24px' }}
+                    onClick={() => props.showSignInPopup('github')}
+                >
+                    Sign in with Github
+                </button>
+            </div>
         </div>
     )
 }
